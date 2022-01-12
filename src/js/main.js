906090a78c7c7b2d1725005bb61becf473806f59
Ninte <ninte.dev@gmail.com>
2022-01-12 16:14:47 +0100

    Setup admin view toggle and account creation sections.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
+// dynamic year setting
 var year = document.getElementById('year');
 year.innerText = new Date().getFullYear();
+
+// toggle admin view
+
+var accountCreation = document.getElementById('accountCreation');
+accountCreation.style.visibility = 'hidden';
+
+var defaultAdminView = false;
+
+function switchAdminView() {
+  if (!defaultAdminView) {
+    console.log('clicked');
+    defaultAdminView = true;
+    accountCreation.style.visibility = 'visible';
+  } else {
+    console.log('not clicked');
+    defaultAdminView = false;
+    accountCreation.style.visibility = 'hidden';
+  }
+}
+
+adminView.onclick = switchAdminView;
