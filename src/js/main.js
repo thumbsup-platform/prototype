556401f19e5aef64a02302b5ca4d4d27eecdf469
Ninte <ninte.dev@gmail.com>
2022-01-28 05:52:52 +0100

    Created training section toggle display.

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,67 @@ year.innerText = new Date().getFullYear();
 
 var accountCreation = document.getElementById('accountCreation');
 accountCreation.style.visibility = 'hidden';
+accountCreation.style.height = '0px';
 
 var defaultAdminView = false;
 
-function switchAdminView() {
+var switchAdminView = () => {
   if (!defaultAdminView) {
-    console.log('clicked');
     defaultAdminView = true;
     accountCreation.style.visibility = 'visible';
+    accountCreation.style.height = 'auto';
   } else {
-    console.log('not clicked');
     defaultAdminView = false;
     accountCreation.style.visibility = 'hidden';
+    accountCreation.style.height = '0px';
   }
-}
+};
 
 adminView.onclick = switchAdminView;
+
+// tab display
+
+var trainingsTrigger = document.getElementById('trainingsTrigger');
+var tabOutput = document.getElementById('tabOutput');
+
+var tabDisplay = document.getElementById('tabDisplay');
+tabDisplay.style.visibility = 'hidden';
+tabDisplay.style.height = '0px';
+
+var defaultTabsView = false;
+
+var switchTabView = () => {
+  if (!defaultTabsView) {
+    defaultTabsView = true;
+    tabDisplay.style.visibility = 'visible';
+    tabDisplay.style.height = 'auto';
+  } else {
+    defaultTabsView = false;
+    tabDisplay.style.visibility = 'hidden';
+    tabDisplay.style.height = '0px';
+  }
+};
+
+trainingsTrigger.onclick = switchTabView;
+
+// trainings display
+
+var tabsQuery = document.querySelectorAll('.tab-select');
+var tabToDisplay = Array.from(tabsQuery);
+
+var displayTab = (id) => {
+  var selectedTab = id.split('_')[0];
+
+  var tabValue = null;
+
+  if (tabOutput.firstChild) {
+    tabOutput.removeChild(tabOutput.firstChild);
+  }
+
+  for (var display of tabToDisplay) {
+    if (display.id.split('_')[0] === selectedTab) {
+      tabValue = display;
+      tabOutput.appendChild(tabValue);
+    }
+  }
+};
